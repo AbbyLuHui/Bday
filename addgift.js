354420df682c996610a0d0923007be38d792aa56
AbbyLuHui <hl3231@columbia.edu>
2020-01-15 10:59:51 -0800

    popup page

---
+++ addgift.js
@@ -0,0 +1,41 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// let changeColor = document.getElementById('changeColor');
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//         tabs[0].id,
+//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
+let giftSubmitButton = document.getElementById('giftSubmitButton');
+console.log("started");
+giftSubmitButton.onclick = function(element){
+  console.log('clicked');
+  var name = document.getElementById('name');
+  let url = document.getElementById('url').value;
+  let description = document.getElementById('description').value;
+  var modified={};
+  var date="";
+  chrome.storage.sync.get(['birthday'], function(data){
+    modified = {"birthday":{"name":data.birthday.name, "date":data.birthday.date, "gifturl":url, "giftdescription":description}};
+  });
+  console.log("--------");
+  console.log(date);
+  console.log(modified);
+  window.location.href = "popup.html";
+  chrome.storage.sync.set(modified, function(){
+    //window.location.href = "popup.html";
+  });
+
+}
--- popup.js
+++ popup.js
@@ -4,17 +4,30 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+// let changeColor = document.getElementById('changeColor');
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
+//
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     chrome.tabs.executeScript(
+//         tabs[0].id,
+//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+//   });
+// };
+let displayBirthday = document.getElementById('displayBirthday');
+chrome.storage.sync.get(['birthday', 'color'], function(data){
+    displayBirthday.rows[0].cells[0].innerHTML = data.birthday.name;
+    displayBirthday.rows[0].cells[1].innerHTML = data.birthday.date;
+    displayBirthday.rows[1].cells[0].innerHTML = data.birthday.gifturl;
+    displayBirthday.rows[1].cells[1].innerHTML = data.birthday.giftdescription;
+    console.log(data.birthday.gifturl);
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
+let addGift = document.getElementById('addGift');
+addGift.onclick = function(element){
+  window.location.href = "addgift.html";
+}
