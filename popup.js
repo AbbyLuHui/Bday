532608cd2868279037d487098394aa58e45af811
AbbyLuHui <hl3231@columbia.edu>
2020-01-17 14:56:21 -0800

    event id

--- popup.js
+++ popup.js
@@ -1,26 +1,8 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
-// let changeColor = document.getElementById('changeColor');
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-//
-// changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.executeScript(
-//         tabs[0].id,
-//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
-//   });
-// };
 let displayBirthday = document.getElementById('displayBirthday');
+//chrome.storage.local.set({"birthday":{}});
 chrome.storage.local.get(['birthday'], function(data){
-    console.log(data);
     var bday = data.birthday;
     var names = Object.keys(bday).sort(function(a,b) {
       return (new Date(bday[a].date) - new Date(bday[b].date))});
