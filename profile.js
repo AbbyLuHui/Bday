7cf960b2723608c24b1dae3e2defc575f7dc1153
AbbyLuHui <hl3231@columbia.edu>
2020-01-17 20:41:37 -0800

    delete func

--- profile.js
+++ profile.js
@@ -34,7 +34,7 @@ for (i = 0; i < closebtns.length; i++) {
   bts[1].addEventListener('click',cancel,false);
   bts[2].addEventListener('click',edit,false);
   bts[3].addEventListener('click', back, false);
-  bts[4].addEventListener('click', del, false)
+  bts[5].addEventListener('click', del, false)
   chrome.storage.local.get("current_friend", function(user){
     var prof_name = D.getElementById("profile_name");
     prof_name.innerHTML = user.current_friend;
@@ -47,7 +47,7 @@ for (i = 0; i < closebtns.length; i++) {
         var node = document.createElement('a');
         node.className = "list-group-item list-group-item-action";
         var linkedText = document.createTextNode(giftdescription[index]);
-        
+
         var span = document.createElement('SPAN');
         span.className = "close";
         var x = document.createTextNode("x");
@@ -75,8 +75,21 @@ for (i = 0; i < closebtns.length; i++) {
        'Authorization' : 'Bearer ' + token,
        'Content-Type': 'application/json'
      })
+     chrome.storage.local.get("current_friend", function(user){
+       chrome.storage.local.get("birthday", function(data){
+         var event_id = data.birthday[user.current_friend].id;
+         const deleteParams = {
+           headers: headers,
+           method: "DELETE",
+         };
+        fetch ('https://www.googleapis.com/calendar/v3/calendars/primary/events/' + event_id, deleteParams);
+        delete data.birthday[user.current_friend];
+        chrome.storage.local.set(data);
+        window.location.href = "popup.html";
 
-   }
+       })
+     })
+   })
  }
  function save(e){
   e.preventDefault();
