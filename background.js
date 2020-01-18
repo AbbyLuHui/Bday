172444bf40cfa56af81db08f64c89d3e0b9e8ca0
AbbyLuHui <hl3231@columbia.edu>
2020-01-18 12:20:08 -0800

    before pull

--- background.js
+++ background.js
@@ -1,23 +1,6 @@
 'use strict';
 
-chrome.contextMenus.create({
-  id: "addbirthday",
-  title: "Add Birthday to Google Calendar", 
-  contexts:["selection"], 
-  onclick: addBirthday
-});
-
-function addBirthday(e){
-  var birthday = e.target.parentElement.getAttribute("data-tooltip-content")
-    chrome.runtime.sendMessage({friend:birthday}, function(response){
-      console.log(response.sent);
-    })
-}
-
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
-  });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
--- content.js
+++ content.js
@@ -1,15 +1,7 @@
-
-
-// chrome.contextMenus.create({
-//   id: "addbirthday",
-//   title: "Add Birthday to Google Calendar", 
-//   contexts:["selection"], 
-//   onclick: addBirthday
-// });
-
-// function addBirthday(e){
-//   var birthday = e.target.parentElement.getAttribute("data-tooltip-content")
-//     chrome.runtime.sendMessage({friend:birthday}, function(response){
-//       console.log(response.sent);
-//     })
-// }
+window.oncontextmenu = function(e) {
+    var birthday = e.target.parentElement.getAttribute("data-tooltip-content")
+    console.log(birthday);
+    chrome.runtime.sendMessage({friend:birthday}, function(response){
+      console.log(response.sent);
+    })
+}
