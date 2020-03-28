2d8a1927b445eec0000b1e9b7c1f45b45cdf7663
Arya Zhao <lz2650@columbia.edu>
2020-03-28 17:41:26 +0800

    second version

--- background.js
+++ background.js
@@ -19,15 +19,6 @@ chrome.runtime.onInstalled.addListener(function() {
       console.log("Set");
     }
   })
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
 });
 
 var event;
