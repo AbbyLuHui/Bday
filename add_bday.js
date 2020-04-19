ddc13390ddd320faa49de4babd57aaaceffb8d2d
Arya Zhao <lz2650@columbia.edu>
2020-04-19 19:02:28 +0800

    add bday submitted response

--- add_bday.js
+++ add_bday.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
     $('#submit').click(function(e){
         e.preventDefault()
+        $('.alert').remove()
         var name = $('#name').val()
         var date = new Date($('#bday').val());
         day = date.getDate();
@@ -16,6 +17,15 @@ $(document).ready(function(){
             createBirthdayEvent(name, calendar_year_bday, curr_year_bday);
           }
         })
+       
+        $('#back').before('<div class="alert alert-success" role="alert">Successfully stored! </div>')
+        $('.alert').append('<button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>')
+        $('.close').click(function(event){
+            event.preventDefault()
+            $('.alert').remove()
+        })
+        $('#name').val('').focus()
+        $('#bday').val('')
     })
     $('#back').click(function(e){
         e.preventDefault()
